45f130258f0d8c9e642c72eb450977e2e06f9031
AnonymerNiklasistanonym <niklas.mikeler@gmail.com>
2021-10-23 20:37:01 +0200

    Fix import of your_package_name

--- src/your_package_name/__init__.py
+++ src/your_package_name/__init__.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
 
 #import your_package_name
-#from .your_package_name import your_package_name
+from your_package_name.your_package_name import main as main_method
 
 
 def main_init():
     print("__init__.py main method was called without any package references")
 
-
-#def main_init():
-#    print("__init__.py main method was called")
-#    your_package_name.main_source()
+def main():
+    main_method()
